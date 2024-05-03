869a50efa3abb49ceb19bd366ea75926d8db509d
6tab9 <tabolator1@gmail.com>
2024-05-03 15:30:29 +0200

    abcdef

--- notesApp/App.js
+++ notesApp/App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import {
+    DrawerContentScrollView,
+    DrawerItemList,
+    DrawerItem
+} from '@react-navigation/drawer';
+import * as SecureStore from 'expo-secure-store';
 
+const Drawer = createDrawerNavigator();
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+      return (
+          <NavigationContainer>
+              <Drawer.Navigator >
+                  <Drawer.Screen name="s1" component={S1}  />
+                  <Drawer.Screen name="s2" component={S2} />
+              </Drawer.Navigator>
+          </NavigationContainer>
+      );
+  }
 
 const styles = StyleSheet.create({
   container: {
